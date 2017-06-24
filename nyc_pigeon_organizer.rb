098710e17d35f6051dee98e4ee6e81b7cdc3f29d
wk645 <wk645@nyu.edu>
2017-06-24 14:06:36 -0400

    pigeon

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    final_results = {}
+    data.each do |characteristic, info|
+        info.each do |traits, names|
+            names.each do |name|
+                if final_results[name] == nil
+                    final_results[name] = {}
+                    final_results[name][characteristic] = [traits.to_s]
+                    elsif final_results[name][characteristic] != nil
+                    final_results[name][characteristic] << traits.to_s
+                    else
+                    final_results[name][characteristic] = [traits.to_s]
+                end
+            end
+        end
+    end
+    final_results
 end
